4369aff1c96ab099a1e7cc1bedf2806531b823c1
gartht <gartht@gmail.com>
2016-02-27 16:29:57 -0500

    added initial super simple server

---
+++ assistant.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe("0.0.0.0:3000", nil)
+}
+
+func indexHandler(response http.ResponseWriter, request *http.Request){
+	fmt.Fprintln(response, "Hello World")
+}
