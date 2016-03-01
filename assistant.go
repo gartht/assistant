6fd19dd37e2f8aa4cded2c6650e3ec3e782ef6ff
gartht <gartht@gmail.com>
2016-03-01 08:24:12 +0000

    Starting to add google tasks api service

--- assistant.go
+++ assistant.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gartht/assistant/taskService"
 )
 
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe("0.0.0.0:3000", nil)
+
 }
 
 func indexHandler(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(response, "Hello World")
+	taskLists := taskService.GetLists()
+	responseTxt := "No lists found"
+	if len(taskLists.Items) > 0 {
+		responseTxt = taskLists.Items[0].Title
+
+	}
+	fmt.Fprintln(response, responseTxt)
+
 }
